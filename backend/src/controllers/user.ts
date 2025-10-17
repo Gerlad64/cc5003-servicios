2107bf08f49e7208f93411e37d9d2a77221eec06
Gerald <gerald@Gerald.local>
2025-10-17 15:17:27 -0300

    feat: se implementó getAll para users

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -2,6 +2,11 @@ import { Request, Response, NextFunction } from "express";
 import User from "../models/user";
 
 
+const getAll = async (req: Request, res: Response) => {
+    const users = await User.find({});
+    res.json(users);
+}
+
 const getById = async (req: Request, res: Response ) => {
     const id = req.params.id;
     const user = await User.findById(id);
@@ -10,4 +15,4 @@ const getById = async (req: Request, res: Response ) => {
     return res.json(user);
 };
 
-export default { getById };
+export default { getAll, getById };
--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,5 +1,8 @@
 import express from 'express'
+import users from '../controllers/user'
 
 const router = express.Router();
+
+router.get('/', users.getAll);
 
 export default router;
