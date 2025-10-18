038e5667750572f0454f09a7ace415754ca3fa0e
Gerald <gerald@gerald.local>
2025-10-18 02:52:06 -0300

    feat: se implementó y configuró la creación de servicios con auth mediante middleware auth.

--- backend/src/controllers/service.ts
+++ backend/src/controllers/service.ts
@@ -1,13 +1,16 @@
 import { Request, Response, NextFunction } from "express";
 import Service from "../models/service";
+import User from "../models/user";
 
 
 
+/** Trae todos los servicios */
 const getAll = async (req: Request, res: Response) => {
    const allServices = await Service.find({});
    res.json(allServices);
 };
 
+/** Trae un servicio por su id */
 const getById = async (req: Request, res: Response) => {
     const id = req.params.id;
     const service = await Service.findById(id);
@@ -16,4 +19,29 @@ const getById = async (req: Request, res: Response) => {
     return res.json(service);
 };
 
-export default { getAll, getById };
+/** Crea un servicio, el usurio debe estar autenticado */
+const createOne = async (req: Request, res: Response) => {
+    const body = req.body;
+    const user = await User.findById(req.userId);
+    if ( !user )
+        return res.status(400).json({ error: " user not found " });
+    else if (! body.content )
+        return res.status(400).json({ error: " content not found " });
+
+    const service = {
+        user_id: user.id,
+        name: body.name,
+        is_delivery: body.is_delivery,
+        delivery_id: body.delivery_scope,
+        on_location: body.on_location,
+        location: body.location,
+        schedule: body.schedule,
+        description: body.description,
+        pricing: body.pricing,
+        contact: body.contact,
+    }
+    const savedService = await new Service(service).save();
+    return res.status(201).json(savedService);
+}
+
+export default { getAll, getById, createOne };
--- backend/src/routes/services.ts
+++ backend/src/routes/services.ts
@@ -1,11 +1,14 @@
 
 import express from 'express'
 /* controllers */
-import service from '../controllers/service'
+import services from '../controllers/service'
+import {authenticate} from "../middlewares/authMiddleware";
 
 const router = express.Router();
 
-router.get('/', service.getAll);
-router.get('/:id', service.getById);
+router.get('/', services.getAll);
+router.get('/:id', services.getById);
+
+router.post('/', authenticate, services.createOne);
 
 export default router;
