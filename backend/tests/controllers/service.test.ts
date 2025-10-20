e3d7efb865eec1c4f5368f5f25a1b6a54ec2e812
Gerald <gerald@gerald.local>
2025-10-20 00:16:16 -0300

    test: se testea si un servicio válido puede ser creado estando autenticado

--- backend/tests/controllers/service.test.ts
+++ backend/tests/controllers/service.test.ts
@@ -2,14 +2,27 @@
 import app from "../../src/app"
 import {test, after, beforeEach, describe, afterEach} from "node:test"
 import assert from "node:assert";
-import initial from '../test_utils';
+import {db, initial} from '../test_utils';
 import supertest from "supertest";
 import mongoose from "mongoose";
 import User from "../../src/models/user";
 import Service from "../../src/models/service";
+import bcrypt from "bcrypt";
 
 const api = supertest(app);
 const base_url = "/api/services"
+
+const login = async (user: { username: string; password: string }) => {
+    const response = await api
+        .post("/api/login")
+        .send(user);
+    const csrfToken = response.headers["x-csrf-token"];
+    const setCookie = response.headers["set-cookie"];
+    assert(Array.isArray(setCookie), "Set-Cookie header missing");
+    const jwtValue = setCookie[0].match(/token=([^;]+)/)?.[1];
+    return { csrfToken, token: jwtValue };
+};
+
 
 describe("When there is initially some services", () => {
     beforeEach(async () => {
@@ -35,9 +48,47 @@ describe("When there is initially some services", () => {
 
 
 
-
-
 });
+
+describe("When there is initially one user logged in", () => {
+    beforeEach(async () => {
+        const passwordHash = await bcrypt.hash("sekret", 10);
+        const newUser = new User({
+            username: "root",
+            password: passwordHash,
+            name: "Root",
+            last_name: "Rumirez",
+        });
+        await newUser.save();
+    });
+
+    afterEach(async () => await api.post("/api/login/logout"));
+
+    test("A valid service can be created", async () => {
+        const initialServices = await db.services();
+        const service = {
+            name: "Clases particulares MATES",
+            is_delivery: true,
+            delivery_scope: ["Estación Central", "Santiago"],
+            on_location: true,
+            location: "Santiago",
+            schedule: "de lunes a viernes de 10:00AM a 21:00PM",
+            description: " Ofrezco clases particulares de matemática todos los niveles",
+            pricing: "10lkas la hora",
+            contact: {whatsapp: "+56912345678"}
+        };
+        const {csrfToken, token} = await login({username: "root", password: "sekret"});
+        const result = await api
+            .post(base_url)
+            .set('Cookie', `token=${token}`)
+            .set('X-CSRF-Token', csrfToken)
+            .send(service)
+            .expect(201);
+        const finalServices = await db.services();
+        assert.strictEqual(finalServices.length, initialServices.length + 1);
+    })
+})
+
 after(async () => {
     await mongoose.connection.close();
 });
