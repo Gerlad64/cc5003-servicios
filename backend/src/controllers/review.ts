3cd745070fb217e2708a8342dfc088dda953fe58
Gerald <gerald@gerald.local>
2025-10-20 23:17:10 -0300

    feat: controladores de review getAll y getByServiceId implementados

--- backend/src/controllers/review.ts
+++ backend/src/controllers/review.ts
@@ -5,11 +5,16 @@ import Review from "../models/review";
 
 
 const getAll = async (req: Request, res: Response) => {
-
+    const allReviews = await Review.find({});
+    res.json(allReviews);
 }
 
 const getByServiceId = async (req: Request, res: Response) => {
-
+    const service_id = req.params.id;
+    const review = await Review.find({service_id: service_id});
+    if (!review)
+        return res.status(404).end();
+    return res.json(review);
 }
 
 const createOne = async (req: Request, res: Response) => {
