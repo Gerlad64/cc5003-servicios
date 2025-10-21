46c6e2c4f11ffcdff26114ae90fd5d2c9d05684a
Gerald <gerald@gerald.local>
2025-10-20 22:45:45 -0300

    docs: modelo ReviewData documentado

--- frontend/src/model/ReviewData.ts
+++ frontend/src/model/ReviewData.ts
@@ -1,8 +1,18 @@
+/**
+ * Interface que representa la reseña hacia un servicio,
+ * creada por un usuario con cuenta registrada.
+ */
 export interface ReviewData {
+    /** id de la reseña */
     id: string;
+    /** id del servicio al que se está creando una reseña */
     service_id: string;
+    /** id del usuario autor de la reseña*/
     user_id: string;
+    /** rating proporcionado */
     rating: number; // e.g., 1 to 5
-    comment: string;
-    created_at: Date; // ISO date string
+    /** comentario opcional de la reseña */
+    comment?: string;
+    /** fecha de creación*/
+    createdAt: Date; // ISO date string
 }   
