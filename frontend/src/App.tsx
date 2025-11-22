b9dba83f27db3fe0e1097efd0fe10235f0ac5e07
Gerald <gerald@local.com>
2025-11-22 17:25:27 -0300

    feat: se implementó restore del login y una topbar provisional en App.tsx cuando se hace restore del login se cambia la variable username por la del username del usuario loggeado Se coloca en una topbar provisional visible por todas las vistas de la app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,11 +7,23 @@ import RegisterForm from './components/RegisterForm';
 import LoginForm from './components/LoginForm';
 import CreateServiceForm from './components/CreateServiceForm';
 import UserPage from './components/UserPage';
+import {useEffect, useState} from "react";
+import loginService from "./requests/login"
 
 function App() {
+   const [username, setUser] = useState<string | null>(null);
+
+    useEffect(() => {
+        const checkLogin = async () => {
+            const user = await loginService.restoreLogin()
+            setUser(user?.username);
+        }
+        checkLogin()
+    }, [])
   return (
     <Router>
       <div className="App">
+          {username ? <h2>esta es una topbar. Usuario: {username}</h2> : <h2>esta es una topbar: usuario no loggeado</h2>}
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/services" element={<Services />} />
