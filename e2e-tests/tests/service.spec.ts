773a7fc5e4d1537c3a048320adcbefe12addccf0
aledzs <alejandro.diaz.mesias@gmail.com>
2025-11-23 05:52:33 -0300

    Feat: e2e test de creacion de sevicio sin estar loggeado

--- e2e-tests/tests/service.spec.ts
+++ e2e-tests/tests/service.spec.ts
@@ -44,6 +44,33 @@ test.describe("Service tests", () => {
     await expect(page.getByText("Santiago")).toBeVisible();
     await expect(page.getByText("$15000 por hora")).toBeVisible();
 
-  })
+  });
+
+  test("Creating a service while logged out fails", async ({ page }) => {
+    await page.goto("/services/create");
+
+    await page.getByLabel("Nombre del servicio").fill("service123");
+    await page.getByLabel("Categoría").fill("Educacion");
+    await page.getByLabel("Descripción").fill("Preparacion PAES");
+    await page.getByLabel("Ubicación").first().fill("Santiago");
+    await page.getByLabel("Horario").fill("Lunes a Sabado 18:00-21:00");
+    await page.getByLabel("Precios").fill("$15000 por hora");
+    await page.getByLabel("¿Ofrece servicio a domicilio?").check();
+    await page.getByLabel("Alcance del delivery (separados por comas)").fill("Santiago Centro, Santiago Oriente");
+    await page.getByLabel("WhatsApp").fill("+56912345678");
+    await page.getByLabel("Instagram").fill("@profesorpaes");
+
+    page.on('dialog', async dialog => {
+      expect(dialog.type()).toBe('alert');
+      expect(dialog.message()).toContain('Error al crear el servicio');
+      await dialog.dismiss(); // Accept the alert
+    });
+    
+    await page.evaluate(() => {
+      document.querySelectorAll('input[type="file"]').forEach(el => el.remove());
+    });
+
+    await page.getByRole("button", { name : "Crear Servicio" }).last().click();
+  });
 
 });
