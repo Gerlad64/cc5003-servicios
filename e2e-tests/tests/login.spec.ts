f7b9b650eca39c9340b8ccaab95fd18393332b5b
aledzs <alejandro.diaz.mesias@gmail.com>
2025-11-23 05:02:39 -0300

    Feat: e2e tests de login fracasado y logout exitoso

--- e2e-tests/tests/login.spec.ts
+++ e2e-tests/tests/login.spec.ts
@@ -21,6 +21,29 @@ test.describe("Login tests", () => {
     await page.getByLabel("Nombre de usuario").fill("esteban.lopez");
     await page.getByLabel("Contraseña").fill("password");
     await page.getByRole("button", { name : "Iniciar Sesión" }).last().click();
+
     await expect(page.getByText("esteban.lopez")).toBeVisible();
+  });
+
+  test("Unsuccessful login", async ({ page }) => {
+    await page.goto("/login");
+
+    await page.getByLabel("Nombre de usuario").fill("esteban.lopezz");
+    await page.getByLabel("Contraseña").fill("password1");
+    await page.getByRole("button", { name : "Iniciar Sesión" }).last().click();
+
+    await expect(page.getByText("Credenciales inválidas. Por favor, intenta nuevamente.")).toBeVisible();
+  });
+
+  test("Successful logout", async ({ page }) => {
+    await page.goto("/login");
+
+    await page.getByLabel("Nombre de usuario").fill("esteban.lopez");
+    await page.getByLabel("Contraseña").fill("password");
+    await page.getByRole("button", { name : "Iniciar Sesión" }).last().click();
+
+    await page.getByRole("button", { name: "Cerrar sesión" }).click();
+
+    await expect(page.getByRole("button", { name: "Iniciar Sesión"}).first()).toBeVisible();
   });
 });
